6e8cd161ddb9e20858ac5bce69140443d8c5de88
Hadekha Erfadila Fitra <hdkef11@gmail.com>
2021-05-13 22:57:04 +0800

    add forever loop

--- main.go
+++ main.go
@@ -21,47 +21,49 @@ func spamTarget(target *string, total *int, delay *int, text *string) {
 		panic("NO TARGET, PLS BUILD NEW SCRIPT")
 	}
 
-	go func() {
-		fmt.Println("endpoint hit ", *target, *total, *delay, *text)
-
-		counter := 0
-
-		for {
-
-			if counter >= *total {
-				break
-			}
-
-			code := rand.Int31()
-
-			emailMe := os.Getenv("EMAIL")
-			pswdMe := os.Getenv("PSWD")
-			host := os.Getenv("HOST")
-			emailTo := []string{*target}
-			port := os.Getenv("SMTPPORT")
-			addr := fmt.Sprintf("%s:%s", host, port)
-			auth := smtp.PlainAuth("", emailMe, pswdMe, host)
-			msgString := "Subject: Hello brother\n\n" + *text + fmt.Sprint(code)
-			msgBytes := []byte(msgString)
-
-			err := smtp.SendMail(addr, auth, emailMe, emailTo, msgBytes)
-
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-
-			fmt.Println("counter : ", counter)
-
-			time.Sleep(time.Duration(*delay) * time.Second)
-
-			counter++
-		}
-
-		fmt.Println("job completed")
-	}()
-
 	fmt.Println("OK. pls wait")
+
+	for {
+		go func() {
+			fmt.Println("endpoint hit ", *target, *total, *delay, *text)
+
+			counter := 0
+
+			for {
+
+				if counter >= *total {
+					break
+				}
+
+				code := rand.Int31()
+
+				emailMe := os.Getenv("EMAIL")
+				pswdMe := os.Getenv("PSWD")
+				host := os.Getenv("HOST")
+				emailTo := []string{*target}
+				port := os.Getenv("SMTPPORT")
+				addr := fmt.Sprintf("%s:%s", host, port)
+				auth := smtp.PlainAuth("", emailMe, pswdMe, host)
+				msgString := "Subject: Hello brother\n\n" + *text + fmt.Sprint(code)
+				msgBytes := []byte(msgString)
+
+				err := smtp.SendMail(addr, auth, emailMe, emailTo, msgBytes)
+
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+
+				fmt.Println("counter : ", counter)
+
+				time.Sleep(time.Duration(*delay) * time.Second)
+
+				counter++
+			}
+
+			fmt.Println("job completed")
+		}()
+	}
 
 }
 
